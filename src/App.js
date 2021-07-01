91e8c2a740debaeb779f786559143878a9ed7b02
elbigans <rhafaelbijaksana04@gmail.com>
2021-07-01 18:54:42 +0700

    memindahkan proses menghitung data keparent component

--- src/App.js
+++ src/App.js
@@ -7,23 +7,162 @@ export default function App() {
   let [actions, setActions] = useState([]);
   let [fullMode, setFullMode] = useState(true);
 
+  let updateFullMode = useCallback(() => {
+    setFullMode(true);
+  }, []);
+
+  useEffect(() => {
+    document.body.addEventListener("keydown", (event) => {
+      const key = event.key,
+      button = document.querySelectorAll('#main button');
+
+      // Cocokan
+      button.forEach((val) => {
+        if(val.textContent === key) val.click()
+      });
+    });
+  }, []);
+
+
+
+  let generateResult = useCallback(function () {
+    let data = [...actions];
+  
+    // berfungsi untuk menampung nilai
+    let number = ``;
+    // Tampung nilai antara operator
+    let numbers = [];
+    // Buat array object yang berisi operation dan nilai `prev` dan `next`
+    let operations = [];
+    // Siapkan variabel untuk 
+    let finalResult = 0;
+    let display = data.length <= 0 ? "0" : "";
+  
+    // Lakukan Pengulangan
+    data.forEach((val) => {
+  
+      // Check apakah Nilai tersebut adalah sebuah operation
+      const isOperation = checkIfSame(val);
+  
+      if (isOperation.done) {
+        // Tambahkan spasi (Jika nilainya adalah operator)
+        display += ` ${val} `;
+        // overwrite numbers terakhir dengan number yang sudah dikumpulkan
+        numbers[numbers.length - 1] = number;
+  
+        // Tambahkan anggota numbers
+        // Karena Jika sudah memasukan operator kita harus membuat penampung yang
+        // baru
+        numbers.push("");
+  
+        // Tambahkan ke operation
+        operations.push({
+          val,
+          prev: numbers.length - 2,
+          next: numbers.length - 1,
+          done: false,
+        });
+  
+        // Reset 
+        number = ``;
+      } else {
+        display += `${val}`;
+        number += val;
+  
+        // Jika masih awal tambahkan string kosong
+        if (numbers.length === 0) numbers.push("");
+  
+        // Jika sebelumnya sudah ditambahkan maka tambahkan
+        if (!isOperation) numbers[numbers.length - 1] += `${val}`;
+      }
+    });
+  
+    
+    // urutkan operasi bedasarkan kukabataku ( kurung, kali , bagi, tambah, kurang)
+    operations = operations.sort((a, b) => {
+      const { type } = checkIfSame(a.val);
+      const { type: type2 } = checkIfSame(b.val);
+  
+      // Compare
+      // Jika sama
+      // expect X > / >  + > -
+      if (type === type2) return 0;
+  
+      // Pada perkalian
+      if (type === "kali") return -1;
+  
+      // Pada Pembagian
+      if (type === "bagi" && type2 !== "kali") return -1;
+  
+      // Pada Penjumlahan
+      if (type === "tambah" && type2 !== "kali" && type2 !== "bagi") return -1;
+  
+      // Pada Pengurangan
+      if (
+        type === "kurang" &&
+        type2 !== "kali" &&
+        type2 !== "bagi" &&
+        type2 !== "tambah"
+      )
+        return -1;
+  
+      return 1;
+    });
+  
+    // Jumlahkan
+    operations.forEach((operation, index) => {
+      let { prev, next } = operation;
+      let { type } = checkIfSame(operation.val);
+      let checkDone = checkIfDone(operations, prev, next, index);
+      let prevValue = checkDone?.prev?.done || parseInt(numbers[prev]);
+      let nextValue = checkDone?.next?.done || parseInt(numbers[next]);
+  
+      if (!Number.isNaN(nextValue)) {
+        switch (type) {
+          case "kali":
+            operation.done = prevValue * nextValue;
+            break;
+          case "bagi":
+            operation.done = prevValue / nextValue;
+            break;
+          case "tambah":
+            operation.done = prevValue + nextValue;
+            break;
+          case "kurang":
+            operation.done = prevValue - nextValue;
+            break;
+        }
+      }
+    });
+  
+    operations.forEach((val) => {
+      if (!val.used) finalResult += val.done;
+    });
+
+    if(operations.length <= 0 ) finalResult = display;
+  
+  
+    return {display, result: finalResult, operations}
+  }, [actions]);
+
 
   let update = useCallback((val) => {
 
     // Check Nilai fullmode 
     if (fullMode === true) {
       let isOperation = checkIfSame(val);
-    
+      let {result: data} = generateResult();
+      
       // Set Full Mode
       setFullMode(false);
 
-      if(isOperation) setActions((prev) => [...prev, val]);
+      if(isOperation) setActions([`${data}`, val]);
       else if(val !== null) setActions(val === false ? [] : [val]);
 
       // Jika null
       if(val === null) {
-        let resultElement = document.getElementById('result');
-        let resultElement2 = resultElement.textContent.split()[0];
+        data = `${data}`
+        let resultElement2 = data.split()[0];
         let final = resultElement2.slice(0, resultElement2.length - 1);
         let result = final.length > 0 ? [...final] : [];
         setActions(result);
@@ -47,26 +186,11 @@ export default function App() {
         });
       }
     }
-  }, [fullMode]);
-
-  let updateFullMode = useCallback(() => {
-    setFullMode(true);
-  }, []);
-
-  useEffect(() => {
-    document.body.addEventListener("keydown", (event) => {
-      const key = event.key,
-      button = document.querySelectorAll('#main button');
-
-      // Cocokan
-      button.forEach((val) => {
-        if(val.textContent === key) val.click()
-      });
-    });
-  }, []);
+  }, [fullMode, generateResult]);
+  
 
   return (
-    <Context.Provider value={{ update, actions, updateFullMode, fullMode }}>
+    <Context.Provider value={{ update, updateFullMode, fullMode, generateResult }}>
       <div className={styleApp.app}>
         <div className={styleApp.heading}>
           <h1>Calculator</h1>
@@ -82,101 +206,14 @@ export default function App() {
 }
 
 function Display(props) {
-  let { actions, fullMode } = useContext(Context);
-  let number = ``;
-  let numbers = [];
-  let operations = [];
-  let finalResult = 0;
-  let value = actions.length <= 0 ? "0" : "";
+  let {generateResult, fullMode } = useContext(Context);
+  let { result, display} = generateResult();
 
-  // Lakukan Pengulangan
-  actions.forEach((val) => {
-    const isOperation = checkIfSame(val);
-    if (isOperation.done) {
-      value += ` ${val} `;
-      numbers[numbers.length - 1] = number;
-      numbers.push("");
-      operations.push({
-        val,
-        prev: numbers.length - 2,
-        next: numbers.length - 1,
-        done: false,
-      });
-      number = ``;
-    } else {
-      value += `${val}`;
-      number += val;
-      if (numbers.length === 0) numbers.push("");
-      if (!isOperation) numbers[numbers.length - 1] += `${val}`;
-    }
-  });
-
-  // urutkan operasi bedasarkan kukabataku ( kurung, kali , bagi, tambah, kurang)
-  operations = operations.sort((a, b) => {
-    const { type } = checkIfSame(a.val);
-    const { type: type2 } = checkIfSame(b.val);
-
-    // Compare
-    // Jika sama
-    // expect X > / >  + > -
-    if (type === type2) return 0;
-
-    // Pada perkalian
-    if (type === "kali") return -1;
-
-    // Pada Pembagian
-    if (type === "bagi" && type2 !== "kali") return -1;
-
-    // Pada Penjumlahan
-    if (type === "tambah" && type2 !== "kali" && type2 !== "bagi") return -1;
-
-    // Pada Pengurangan
-    if (
-      type === "kurang" &&
-      type2 !== "kali" &&
-      type2 !== "bagi" &&
-      type2 !== "tambah"
-    )
-      return -1;
-
-    return 1;
-  });
-
-  // Jumlahkan
-  operations.forEach((operation, index) => {
-    let { prev, next } = operation;
-    let { type } = checkIfSame(operation.val);
-    let checkDone = checkIfDone(operations, prev, next, index);
-    let prevValue = checkDone?.prev?.done || parseInt(numbers[prev]);
-    let nextValue = checkDone?.next?.done || parseInt(numbers[next]);
-
-    if (!Number.isNaN(nextValue)) {
-      switch (type) {
-        case "kali":
-          operation.done = prevValue * nextValue;
-          break;
-        case "bagi":
-          operation.done = prevValue / nextValue;
-          break;
-        case "tambah":
-          operation.done = prevValue + nextValue;
-          break;
-        case "kurang":
-          operation.done = prevValue - nextValue;
-          break;
-      }
-    }
-  });
-
-  operations.forEach((val) => {
-    if (!val.used) finalResult += val.done;
-  });
   
-
   return (
     <div className={styleApp.display}>
-      {!fullMode && <div>{value}</div>}
-      <div id="result">{operations.length > 0 ? finalResult : value}</div>
+      {!fullMode && <div>{display}</div>}
+      <div id="result">{result}</div>
     </div>
   );
 }
